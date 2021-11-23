7d177a3b54ab5a186d226ddee2e58f53291f8712
Arthur Boeira <39688792+Artboeira@users.noreply.github.com>
2021-11-23 07:26:59 -0300

    criei o componente scale

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import MasterBar from './components/MasterBar';
+import InputSlider_scale from './components/InputSlider_scale';
 
 const theme = createTheme({
   palette: {
@@ -26,6 +27,10 @@ function App() {
       <header>
       <MasterBar></MasterBar>
       </header>
+
+      <body>
+        <InputSlider_scale></InputSlider_scale>
+      </body>
     </div>
     </ThemeProvider>
   );
---
+++ src/components/InputSlider_scale.js
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Slider from '@mui/material/Slider';
+import MuiInput from '@mui/material/Input';
+import AspectRatio from '@mui/icons-material/AspectRatio';
+
+const Input = styled(MuiInput)`
+  width: 42px;
+`;
+
+export default function InputSlider_scale() {
+  const [value, setValue] = React.useState(30);
+
+  const handleSliderChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value === '' ? '' : Number(event.target.value));
+  };
+
+  const handleBlur = () => {
+    if (value < 0) {
+      setValue(0);
+    } else if (value > 100) {
+      setValue(100);
+    }
+  };
+
+  return (
+    <Box className = "slider" >
+      <Grid container spacing={2} alignItems="center">
+        <Grid item>
+      <Typography id="input-slider" gutterBottom>
+        Scale
+      </Typography>
+        </Grid>
+        <Grid item>
+          <AspectRatio />
+        </Grid>
+        <Grid item xs>
+          <Slider
+            value={typeof value === 'number' ? value : 0}
+            onChange={handleSliderChange}
+            aria-labelledby="input-slider"
+          />
+        </Grid>
+        <Grid item>
+          <Input
+            value={value}
+            size="small"
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+            inputProps={{
+              step: 10,
+              min: 0,
+              max: 100,
+              type: 'number',
+              'aria-labelledby': 'input-slider',
+            }}
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
